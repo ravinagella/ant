63dde92855495d01f8b37e733480531822d886c8
ravinagella <ravinagella4791@gmail.com>
2017-03-30 08:06:40 +0530

    testing

--- compile/src/MyHelloWorld.java
+++ compile/src/MyHelloWorld.java
@@ -1,5 +1,6 @@
 public class MyHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, This for ANT testing!");
+        System.out.println("Hello World, This for ANT compile!");
     }
 }
